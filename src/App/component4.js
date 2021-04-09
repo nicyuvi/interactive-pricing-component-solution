6f90613363978b4baeecd1f1fd892945b8feb637
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-04-08 20:46:22 -0700

    Add check icon

--- src/App/component4.js
+++ src/App/component4.js
@@ -1,19 +1,20 @@
 import React from 'react';
+import iconCheck from '../images/icon-check.svg';
 
 function Component4() {
   return (
     <>
       <div>
         <div>
-          <img src='../images/icon-check.svg' alt='check icon' />
+          <img src={iconCheck} alt='check icon' />
           <p>Unlimited websites</p>
         </div>
         <div>
-          <img src='../images/icon-check.svg' alt='check icon' />
+          <img src={iconCheck} alt='check icon' />
           <p>100% data ownership</p>
         </div>
         <div>
-          <img src='../images/icon-check.svg' alt='check icon' />
+          <img src={iconCheck} alt='check icon' />
           <p>Email reports</p>
         </div>
       </div>
