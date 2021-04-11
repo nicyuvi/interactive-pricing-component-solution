0ed21d1114f8f0808544c7fb47f55e1232bc7c3d
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-04-10 23:13:30 -0700

    Style header section

--- src/App/HeaderText.js
+++ src/App/HeaderText.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import patternCircle from '../images/pattern-circles.svg';
 
 function HeaderText() {
   return (
-    <>
-      <h1>simple, traffic-based pricing</h1>
+    <div className='header-container main-container'>
+      <img
+        className='header-container__image'
+        src={patternCircle}
+        alt='circle pattern'
+      />
+      <h1>Simple, traffic-based pricing</h1>
       <p>Sign-up for our 30-day trial. </p>
       <p>No credit card required.</p>
-    </>
+    </div>
   );
 }
 
