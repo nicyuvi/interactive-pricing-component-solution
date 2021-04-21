c46ff315c0014eb77853a67a209898ef6244c9c4
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-04-20 22:03:48 -0700

    Style billing toggle

--- src/App/Billing.js
+++ src/App/Billing.js
@@ -3,11 +3,13 @@ import React from 'react';
 function Billing() {
   return (
     <div className='flex-grid-thirds billing-container pricing-container__group'>
-      <p className='col placeholder'></p>
-      <p className='col billing-container__text'>Monthly Billing</p>
-      <p className='col billing-container__toggle'>toggle</p>
-      <p className='col billing-container__text'>Yearly Billing</p>
-      <p className='col billing-container__discount'>-25%</p>
+      <p className='col-1 placeholder'></p>
+      <p className='col-2 billing-container__text'>Monthly Billing</p>
+      <p className='col-1 billing-container__toggle'>
+        <div className='billing-container__toggle-btn'></div>
+      </p>
+      <p className='col-2 billing-container__text'>Yearly Billing</p>
+      <p className='col-1 billing-container__discount'>-25%</p>
     </div>
   );
 }
