5ed3bc43737836ef0e8bc01cb522cb339f67c423
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-04-21 19:58:11 -0700

    Add initial slider styles and functionality

--- src/App/Slider.js
+++ src/App/Slider.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 
 function Slider() {
+  // set slider value state
+  const [sliderValue, setSliderValue] = useState(3);
+
+  // update sliderValue state when slider value changes
+  function sliderCallBack(e) {
+    setSliderValue(e.target.value);
+  }
+
+  // do something when slider value changes
+  useEffect(() => {
+    console.log(sliderValue, 'value changing');
+  }, [sliderValue]);
+
   return (
     <>
       <p className='pricing-container__group'>100K PAGEVIEWS</p>
-      <p className='pricing-container__group'>slider</p>
+      <div className='pricing-container__group slide-container'>
+        <input
+          type='range'
+          min='1'
+          max='5'
+          value={sliderValue}
+          onChange={sliderCallBack}
+          className='slider'
+          id='slider'
+        ></input>
+      </div>
       <p className='pricing-container__group'>
         <span className='pricing-container__price'>$16.00</span> / month
       </p>
