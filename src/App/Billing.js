8465581b4ff2f262dc55b77e077375ac474c7cc4
Nicolis Yuvienco <nickyuvienco@gmail.com>
2021-04-21 19:30:32 -0700

    Fix div cannot be a descendant of p

--- src/App/Billing.js
+++ src/App/Billing.js
@@ -5,9 +5,9 @@ function Billing() {
     <div className='flex-grid-thirds billing-container pricing-container__group'>
       <p className='col-1 placeholder'></p>
       <p className='col-2 billing-container__text'>Monthly Billing</p>
-      <p className='col-1 billing-container__toggle'>
+      <div className='col-1 billing-container__toggle'>
         <div className='billing-container__toggle-btn'></div>
-      </p>
+      </div>
       <p className='col-2 billing-container__text'>Yearly Billing</p>
       <p className='col-1 billing-container__discount'>-25%</p>
     </div>
